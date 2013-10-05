9e3c7ff6aa124be553e33ea6bf6679a78cd3a21f
Winton Welsh <mail@wintoni.us>
2013-10-05 14:53:09 -0700

    Splitting up LighthouseProject#update_from_api! as instructed by CodeClimate

--- app/models/lighthouse_project.rb
+++ app/models/lighthouse_project.rb
@@ -16,38 +16,45 @@ class LighthouseProject < ActiveRecord::Base
   end
 
   def update_from_api!(page=1, limit=100)
-    api_tickets  = lighthouse.recently_updated_tickets(page, limit)
-    tickets_hash = Ticket.hash_tickets_by_numbers(api_tickets.collect(&:number))
-    users_hash   = hash_lighthouse_users_by_lighthouse_id
-
-    next_page =
-      if api_tickets.last
-        last_ticket = tickets_hash[api_tickets.last.number]
-
-        if last_ticket
-          last_ticket.needs_update?(api_tickets.last)
-        else
-          true
-        end
-      end
+    @api_tickets  = lighthouse.recently_updated_tickets(page, limit)
+    @tickets_hash = Ticket.hash_tickets_by_numbers(@api_tickets.collect(&:number))
+    @users_hash   = hash_lighthouse_users_by_lighthouse_id
     
-    api_tickets.each do |api_ticket|
-      assigned = users_hash[api_ticket.assigned_user_id]
-      ticket   = tickets_hash[api_ticket.number]
-      user     = users_hash[api_ticket.user_id]
-      
-      if ticket
-        ticket.assign_attributes(api_ticket.to_attributes)
-      else
-        ticket = tickets.build(api_ticket.to_attributes)
-      end
-
-      ticket.assigned_lighthouse_user = assigned  if assigned
-      ticket.lighthouse_user          = user      if user
-
-      ticket.save
+    @api_tickets.each do |api_ticket|
+      update_ticket_from_api(api_ticket)
     end
 
-    update_from_api!(page + 1, limit)  if next_page
+    update_from_api!(page + 1, limit)  if next_page?
+  end
+
+  private
+
+  def next_page?
+    if @api_tickets.last
+      last_ticket = @tickets_hash[@api_tickets.last.number]
+
+      if last_ticket
+        last_ticket.needs_update?(@api_tickets.last)
+      else
+        true
+      end
+    end
+  end
+
+  def update_ticket_from_api(api_ticket)
+    assigned = @users_hash[api_ticket.assigned_user_id]
+    ticket   = @tickets_hash[api_ticket.number]
+    user     = @users_hash[api_ticket.user_id]
+    
+    if ticket
+      ticket.assign_attributes(api_ticket.to_attributes)
+    else
+      ticket = tickets.build(api_ticket.to_attributes)
+    end
+
+    ticket.assigned_lighthouse_user = assigned  if assigned
+    ticket.lighthouse_user          = user      if user
+
+    ticket.save
   end
 end
