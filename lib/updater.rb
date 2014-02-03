b70c6b863a397d46dc408fddc9ec9d2605277e41
Winton Welsh <mail@wintoni.us>
2014-02-02 18:52:51 -0800

    Simplifying Updater

--- lib/updater.rb
+++ lib/updater.rb
@@ -1,17 +1,28 @@
 module Updater
+  class <<self
 
-  def self.included(base)
-    updaters = Dir["#{Rails.root}/lib/updaters/*"].map do |path|
-      require path
-      type = File.basename(path, ".rb")
-      base.include "Updaters::#{type.classify}".constantize
-      type
+    def included(base)
+      updaters = read_updaters
+      updaters.each do |updater|
+        base.include "Updaters::#{updater.classify}".constantize
+      end
+
+      define_update_method(base, updaters)
     end
 
-    base.send(:define_method, :update_from_changes) do
-      updaters.each do |type|
-        method = "update_from_#{type}"
-        send(method)  if send(method + "?")
+    def define_update_method(base, updaters)
+      base.send(:define_method, :update_from_changes) do
+        updaters.each do |type|
+          method = "update_from_#{type}"
+          send(method)  if send(method + "?")
+        end
+      end
+    end
+
+    def read_updaters
+      Dir["#{Rails.root}/lib/updaters/*"].map do |path|
+        require path
+        File.basename(path, ".rb")
       end
     end
   end
