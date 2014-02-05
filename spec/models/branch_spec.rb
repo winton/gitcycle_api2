cd0baa07a02ec9d0f59f0f7e1aaabd9a4769d19b
Winton Welsh <mail@wintoni.us>
2014-02-04 18:37:02 -0800

    Fixing specs

--- spec/models/branch_spec.rb
+++ spec/models/branch_spec.rb
@@ -16,8 +16,8 @@ describe Branch do
       Github.any_instance.stub(:user).
         and_return(name: "Github Name")
       
-      Github.any_instance.should_receive(:repo).
-        with().and_return(parent: {
+      Github.any_instance.stub(:repo).
+        and_return(parent: {
           owner: {
             login: "owner_login",
             name:  "Owner Name"
@@ -28,20 +28,27 @@ describe Branch do
         and_return(ref: true)
     end
 
-    def branch_expectations(branch, name_and_title=true)
-      if name_and_title
+    def branch_expectations(branch, options={})
+      options = {
+        name_and_title: true,
+        source_branch:  false
+      }.merge(options)
+
+      if options[:name_and_title]
         branch.name.should  == "hello-world"
         branch.title.should == "Hello, world!"
+      end
+      
+      if options[:source_branch]
+        branch.source_branch.name.should            == "master"
+        branch.source_branch.repo.name.should       == "repo:name"
+        branch.source_branch.repo.user.login.should == "owner_login"
+        branch.source_branch.repo.user.name.should  == "Owner Name"
       end
 
       branch.repo.name.should       == "repo:name"
       branch.repo.user.login.should == "repo:user:login"
       branch.repo.user.name.should  == "Github Name"
-
-      branch.source_branch.attributes.values.uniq.should == [ nil ]
-      branch.source_branch.repo.name.should       == "repo:name"
-      branch.source_branch.repo.user.login.should == "owner_login"
-      branch.source_branch.repo.user.name.should  == "Owner Name"
       
       branch.user.login.should == "user:login"
       branch.user.name.should  == "user:name"
@@ -50,7 +57,18 @@ describe Branch do
     context "with minimum parameters" do
       it "initializes records" do
         branch = Branch.find_from_params(params, user)
-        branch_expectations(branch, false)
+        branch_expectations(branch, name_and_title: false)
+      end
+    end
+
+    context "with source branch name" do
+      it "initializes records" do
+        params[:source_branch] = { name: "master" }
+        branch = Branch.find_from_params(params, user)
+        branch_expectations(branch,
+          name_and_title: false,
+          source_branch:  true
+        )
       end
     end
 
