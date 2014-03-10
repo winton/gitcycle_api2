a95c9bea67a9cfd170f51cf7222331554418fce4
Winton Welsh <mail@wintoni.us>
2014-03-10 15:22:02 -0700

    UpdateBranch minor fixes

--- spec/lib/update_branch_spec.rb
+++ spec/lib/update_branch_spec.rb
@@ -2,17 +2,17 @@ require 'spec_helper'
 
 describe UpdateBranch do
 
-  let(:branch) { double(:branch) }
-  subject      { UpdateBranch.new(branch) }
+  let(:branch)        { double(:branch) }
+  let(:update_branch) { UpdateBranch.new(branch) }
+  subject             { update_branch }
 
   describe "#update" do
 
     let(:updater)       { double(:updater, update?: true, update: true) }
     let(:updater_class) { double(:updater, new: updater) }
 
-    before  { allow(UpdateBranch).to receive(:updaters).and_return([ updater_class ]) }
-    before  { subject }
-    subject { UpdateBranch.new(branch).update }
+    before   { allow(UpdateBranch).to receive(:updaters).and_return([ updater_class ]) }
+    subject! { update_branch.update }
 
     specify { expect(updater_class).to have_received(:new).with(branch) }
     specify { expect(updater).to have_received(:update) }
