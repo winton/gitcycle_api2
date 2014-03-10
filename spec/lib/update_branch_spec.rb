4bbea6e322c14b05860a9e38b426d4255723baf6
Winton Welsh <mail@wintoni.us>
2014-03-10 15:02:27 -0700

    UpdateBranch spec return value

--- spec/lib/update_branch_spec.rb
+++ spec/lib/update_branch_spec.rb
@@ -2,18 +2,21 @@ require 'spec_helper'
 
 describe UpdateBranch do
 
-  subject { UpdateBranch.new(branch) }
+  let(:branch) { double(:branch) }
+  subject      { UpdateBranch.new(branch) }
 
   describe "#update" do
 
-    let(:branch)        { double(:branch) }
     let(:updater)       { double(:updater, update?: true, update: true) }
     let(:updater_class) { double(:updater, new: updater) }
 
-    before { allow(subject.class).to receive(:updaters).and_return([ updater_class ]) }
-    before { subject.update }
+    before  { allow(UpdateBranch).to receive(:updaters).and_return([ updater_class ]) }
+    before  { subject }
+    subject { UpdateBranch.new(branch).update }
 
     specify { expect(updater_class).to have_received(:new).with(branch) }
     specify { expect(updater).to have_received(:update) }
+
+    it { should == branch }
   end
 end
